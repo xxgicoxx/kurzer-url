d986c76a1bcc6efdd7a4393bf43ab62b42182c4c
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2020-02-12 00:40:55 -0300

    Change to async

--- src/kurzer-url.js
+++ src/kurzer-url.js
@@ -1,18 +1,14 @@
-const request = require('request-promise');
+const request = require('request-promise-native');
 
-short = (url) => {
-	return new Promise((resolve, reject) => {
-		request({
-			url: `https://is.gd/create.php?format=simple&url=${url}`,
-			json: true
-		}).then((response) => {
-			resolve(response);
-		}).catch((error) => {
-			reject(error);
-		});
-	});
+async function short(url) {
+  try {
+    const shortUrl = await request({ url: `https://is.gd/create.php?format=simple&url=${url}`, json: true });
+    return shortUrl;
+  } catch (ex) {
+    throw new Error(ex);
+  }
 }
 
 module.exports = {
-	short: short
-}
+  short,
+};
